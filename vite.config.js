597181a6dbe17333b07bcb457fa9507abae3e684
Chavvv <chavaughnwilkins@gmail.com>
2023-04-09 17:01:57 -0500

    ex 1

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    server: {
+        open: '/',
+        port: 2921,
+    },
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+
+        }
+    },
+    server: {
+        proxy: {
+            '^/api*': {
+                target: 'http://localhost:8080/'
+            }
+        }
     }
-  }
 })
